1b7749e5636fe06ea3964e6a44cee252bfe0d538
ervikassingh <mail.ervikassingh@gmail.com>
2025-08-11 18:46:00 +0530

    feat: Enhance Home, Contact, and Projects pages with responsive design updates; adjust layout and spacing for improved user experience

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -52,8 +52,35 @@ export default function Example() {
 				</div>
 				<div className="w-full h-px bg-zinc-800" />
 
+				{/* Small screens: 1 column */}
+				<div className="grid grid-cols-1 gap-4 mx-auto md:hidden">
+					{socials.map((s) => (
+						<Card key={s.label}>
+							<Link
+								href={s.href}
+								target="_blank"
+								className="group h-full block"
+							>
+								<article className="p-4 md:p-8 h-full flex flex-col justify-start">
+									<div className="flex items-center justify-center w-12 h-12 md:w-16 md:h-16 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange mb-4">
+										{s.icon}
+									</div>
+									<div className="text-center">
+										<h3 className="text-xl font-medium duration-1000 lg:text-2xl text-zinc-200 group-hover:text-white font-display">
+											{s.label}
+										</h3>
+										<p className="mt-2 text-sm duration-1000 text-zinc-400 group-hover:text-zinc-200">
+											{s.handle}
+										</p>
+									</div>
+								</article>
+							</Link>
+						</Card>
+					))}
+				</div>
+
 				{/* Medium screens: 2 columns */}
-				<div className="grid grid-cols-1 gap-4 mx-auto lg:hidden md:grid-cols-2">
+				<div className="hidden md:grid grid-cols-2 gap-4 mx-auto lg:hidden">
 					{Array.from({ length: 2 }, (_, columnIndex) => (
 						<div key={columnIndex} className="grid grid-cols-1 gap-4">
 							{socials
