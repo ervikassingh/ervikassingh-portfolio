735fdc439e073c6436abd3cbea9bf26af9ffbde6
ervikassingh <mail.ervikassingh@gmail.com>
2025-08-11 17:18:46 +0530

    feat: Add Ethers.js and Hardhat tools to the tools section, and restore C language entry in the languages list with updated links and icons

--- content/languages/index.ts
+++ content/languages/index.ts
@@ -15,14 +15,14 @@ const languages = [
     link: 'https://docs.soliditylang.org/',
   },
   {
-    name: 'C',
-    icon: 'https://cdn.simpleicons.org/c/ffffff',
-    link: 'https://www.iso.org/standard/74528.html',
-  },
-  {
     name: 'C++',
     icon: 'https://cdn.simpleicons.org/cplusplus/ffffff',
     link: 'https://isocpp.org/',
+  },
+  {
+    name: 'C',
+    icon: 'https://cdn.simpleicons.org/c/ffffff',
+    link: 'https://www.iso.org/standard/74528.html',
   },
 ];
 
